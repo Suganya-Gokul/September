57a6b99e37168623bb2ea45546dc78a6375f3f5c
Suganya-Gokul <suganya.vidhu97@gmail.com>
2024-09-12 15:04:47 +0530

    Sep11

---
+++ src/Sept11/Datatypes/Prog001.java
@@ -0,0 +1,24 @@
+package Sept11.Datatypes;
+
+public class Prog001 {
+    public static void main(String[] args) {
+        byte a=115;
+        short b=250;
+        int c=15000;
+        long d=973569395l;
+        char e = 'S';
+
+        float f=15.98f;
+        double g=326.98748192;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+        System.out.println(g);
+
+
+
+    }
+}
