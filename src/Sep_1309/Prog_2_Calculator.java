60d18576d5b4b948f7a3101afde0a2a9c4c2e8dc
Suganya-Gokul <suganya.vidhu97@gmail.com>
2024-09-13 18:01:51 +0530

    Sep11

---
+++ src/Sep_1309/Prog_2_Calculator.java
@@ -0,0 +1,19 @@
+package Sep_1309;
+
+public class Prog_2_Calculator {
+    public static void main(String[] args) {
+        double a=34;
+        double b=10;
+        double c=a+b;
+        double d=a-b;
+        double e=a*b;
+        double f=a/b;
+        double g=a%b;
+
+        System.out.println("Sum of a and b is:" +c);
+        System.out.println("Subraction of a and b is:" +d);
+        System.out.println("Multiplication of a and b is:" +e);
+        System.out.println("Division of a and b is:" +f);
+        System.out.println("Modulus of a and b is:" +g);
+    }
+}
